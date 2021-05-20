eea37f689fea89a93bd3258db68166c284807980
GaryRoberts <garyroberts1995@gmail.com>
2021-05-20 14:57:16 -0500

    Updates

---
+++ precache-manifest.fb3c17e4a415e1e0e778c4e2b9dbc289.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dd1dd1053062c44a70117f107de27b9",
+    "url": "/reactjs-portfolio-site/index.html"
+  },
+  {
+    "revision": "487712cf9b868a0238c2",
+    "url": "/reactjs-portfolio-site/static/css/2.58bc1d28.chunk.css"
+  },
+  {
+    "revision": "48888b2768b0bfd9a4de",
+    "url": "/reactjs-portfolio-site/static/css/main.9cb6c0a0.chunk.css"
+  },
+  {
+    "revision": "487712cf9b868a0238c2",
+    "url": "/reactjs-portfolio-site/static/js/2.356a6022.chunk.js"
+  },
+  {
+    "revision": "6d98c309ab0e4d5aa5d30f35f2390953",
+    "url": "/reactjs-portfolio-site/static/js/2.356a6022.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48888b2768b0bfd9a4de",
+    "url": "/reactjs-portfolio-site/static/js/main.ab4d4529.chunk.js"
+  },
+  {
+    "revision": "4462cee8e9476403958e",
+    "url": "/reactjs-portfolio-site/static/js/runtime-main.5b386ce6.js"
+  }
+]);
